2eb85718d7af3ba22dc4a565899fcc188a70906d
“mgsweet” <mgsweet@126.com>
2017-08-30 14:07:42 +0800

    删除无用函数

--- pages/discountCoupon/couponCenter/couponCenter.js
+++ pages/discountCoupon/couponCenter/couponCenter.js
@@ -15,55 +15,6 @@ Page({
     this.getAvailableCoupons();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
-
   //测试用函数
   getAvailableCoupons: function() {
     //couponStatus 0: 立即使用, 1: 点击领取, 2:已使用, 3:已过期
